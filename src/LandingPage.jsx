4833886825593f0b0ec3181df846ba1c663e9160
Fariandi Ramadhan <fariandi.ramadhann@gmail.com>
2024-05-29 08:36:01 +0700

    add(config)(common)(dev):adding tailwindcss autoprefixer and post css, adding installation note, creating basic structure

---
+++ src/LandingPage.jsx
@@ -0,0 +1,23 @@
+export default function LandingPage(){
+    return(
+        <>
+        {/* This structure will be implemented soon*/}
+        {/* Tailwind will be used for styling this web */}
+        <nav></nav>
+        <header></header>
+        <main>
+            <article>
+                <section>
+                    <h1 className="text-4xl text-yellow-400">section1</h1>
+                </section>
+            </article>
+            <article>
+                <section></section>
+            </article>
+        </main>
+        <footer>
+            <h3>this is footer</h3>
+        </footer>
+        </>
+    )
+}
